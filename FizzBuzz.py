87e4a40c9e06c51fc8af7340b883455649f4627d
GabeS1 <geshz8@umsystem.edu>
2023-03-09 18:03:37 -0600

    forgot n+1 for user to implement their side

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,7 +2,7 @@ n = -1
 while(n < 0):
     n = int(input("Enter max POSITIVE amount of numbers to Fizzbuzz: "))
 
-for i in range (1,100):
+for i in range (1,n+1):
     if i % 3 == 0:
         print("Fizz")
     elif i % 5 == 0:
